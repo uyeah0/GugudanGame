ab916df945a0bb3c4740c0968a05f99e53856e10
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-06-08 18:22:25 +0900

    Update GuGuDan.java

--- 구구단/gugudan/src/PJgugudan/GuGuDan.java
+++ 구구단/gugudan/src/PJgugudan/GuGuDan.java
@@ -2,20 +2,30 @@ package PJgugudan;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
+import java.util.Scanner;
 public class GuGuDan {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		//System.out.println(1);
-		
-		int random = (int)Math.random()*100+1;
-		for(int i = 1; i < 10; i++) {
-			for(int j = 1; j < 10; j++) {
-				System.out.println( i + " * " + j + " = " +i*j);
+		int random,random2;
+		int score = 0;
+		while( score < 100) {
+			random = (int)(Math.random()*9)+2;
+			random2 = (int)(Math.random()*9)+2;
+			System.out.println( random + " * " + random2 + " ? " );
+			int result = sc.nextInt();
+			if(random*random2 == result) {
+				score += 10;
+				System.out.println("현재 스코어 : " + score);
+			}else {
+				System.out.println("틀렸습니다");
 			}
+			
 		}
-//		Timer timerS = new Timer();
+		
+		
+		Timer timerS = new Timer();
 //		
 //		
 //		
